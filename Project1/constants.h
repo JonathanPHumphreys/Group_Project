da69deeec97f98737fb93569abaa709b9e2ae3d7
Yizak <isaacstarkie@hotmail.co.uk>
2017-03-15 20:53:55 +0000

    Adding "include\" back (i'm testing a batch file)

--- Project1/constants.h
+++ Project1/constants.h
@@ -1,10 +1,10 @@
 #pragma once
 using namespace std;
 
-#include "SDL.h"
-#include "SDL_image.h"
-#include "SDL_ttf.h" 
-#include "SDL_mixer.h"
+#include "include\SDL.h"
+#include "include\SDL_image.h"
+#include "include\SDL_ttf.h" 
+#include "include\SDL_mixer.h"
 
 #include <chrono>
 #include <string>
